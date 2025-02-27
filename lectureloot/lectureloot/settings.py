35763e131a50ad713470286422a935474a7280c3
Alex Carr <alexcarr1113@outlook.com>
2025-02-27 17:42:40 +0000

    Added template dir

--- lectureloot/lectureloot/settings.py
+++ lectureloot/lectureloot/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'lectureloot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
