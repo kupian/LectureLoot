58589d55b195f37b2fabe610c3be876967c5ed52
Alex Carr <alexcarr1113@outlook.com>
2025-03-03 13:10:13 +0000

    Created bid model

--- lectureloot/app/models.py
+++ lectureloot/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    pass
+class Listing(models.Model):
+    pass
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignObject(User, on_delete=models.CASCADE, blank=False)
+    amount = models.DecimalField(blank=False)
+    time = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return f"Bid on {self.listing} for £{self.amount}"
+    
