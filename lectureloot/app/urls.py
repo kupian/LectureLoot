90cd9a8b30b36befdc3f9637d042648e90fd9b34
sopstock <156322543+sopstock@users.noreply.github.com>
2025-03-03 15:56:32 +0000

    Add admin to urls.py

--- lectureloot/app/urls.py
+++ lectureloot/app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from app import views
+from django.contrib import admin
 
 app_name = 'app'
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('admin/', admin.site.urls),
 ]
