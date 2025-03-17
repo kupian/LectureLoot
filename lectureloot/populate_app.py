e7f4c40c51966d7983170aff1c0bf3d5f09e50bd
Anya Graffuis <2660508g@student.gla.ac.uk>
2025-03-17 15:54:23 +0000

    Update population script to handle new model changes: remove price field and add description support

--- lectureloot/populate_app.py
+++ lectureloot/populate_app.py
@@ -6,39 +6,39 @@ django.setup()
 from app.models import Category, Listing, CustomUser
 
 def populate():
-    
+    # create or get the seller
     seller = CustomUser.objects.get_or_create(username = "johndoe", email="john@doe.com")[0]
     seller.save()
     seller_id = seller.pk
-
+    # update data dictionaries with the new "description" field.
     stationery_pages = [
-    {'title': 'Pencil', 'url': 'http://stationaryURL1/', 'price': 1.50, 'seller_id': seller_id},
-    {'title': 'Highlighter', 'url': 'http://stationaryURL2/', 'price': 2.00, 'seller_id': seller_id}
+    {'title': 'Pencil', 'url': 'http://stationaryURL1/', 'seller_id': seller_id, 'description': 'A high-quality pencil'},
+    {'title': 'Highlighter', 'url': 'http://stationaryURL2/', 'seller_id': seller_id, 'description': 'Bright highlighter for notes'}
     ]
 
     tech_pages = [
-        {'title': 'Computer', 'url': 'http://www.techURL1/', 'price': 899.99, 'seller_id': seller_id},
-        {'title': 'Headphones', 'url': 'http://www.techURL2/', 'price': 149.99, 'seller_id': seller_id}
+        {'title': 'Computer', 'url': 'http://www.techURL1/', 'seller_id': seller_id, 'description': 'Powerful desktop computer'},
+        {'title': 'Headphones', 'url': 'http://www.techURL2/', 'seller_id': seller_id, 'description': 'Noise-cancelling headphones'}
     ]
 
     toys_pages = [
-        {'title': 'Ball', 'url': 'http://toysURL1', 'price': 10.00, 'seller_id': seller_id},
-        {'title': 'Play-Dough', 'url': 'http://toysURL2', 'price': 5.50, 'seller_id': seller_id}
+        {'title': 'Ball', 'url': 'http://toysURL1', 'seller_id': seller_id, 'description': 'Bouncy ball for play'},
+        {'title': 'Play-Dough', 'url': 'http://toysURL2', 'seller_id': seller_id, 'description': 'Colorful play-dough'}
     ]
 
     entertainment_pages = [
-        {'title': 'Puzzle', 'url': 'http://entertainmentURL1', 'price': 15.99, 'seller_id': seller_id},
-        {'title': 'Nintendo Switch', 'url': 'http://entertainmentURL2', 'price': 299.99, 'seller_id': seller_id}
+        {'title': 'Puzzle', 'url': 'http://entertainmentURL1', 'seller_id': seller_id, 'description': 'Challenging jigsaw puzzle'},
+        {'title': 'Nintendo Switch', 'url': 'http://entertainmentURL2', 'seller_id': seller_id, 'description': 'Popular gaming console'}
     ]
 
     books_pages = [
-        {'title': 'Harry Potter', 'url': 'http://booksURL1', 'price': 12.99, 'seller_id': seller_id},
-        {'title': 'Java for Beginners', 'url': 'http://booksURL2', 'price': 29.99, 'seller_id': seller_id}
+        {'title': 'Harry Potter', 'url': 'http://booksURL1', 'seller_id': seller_id, 'description': 'Fantasy novel series'},
+        {'title': 'Java for Beginners', 'url': 'http://booksURL2', 'seller_id': seller_id,'description': 'Introductory programming book' }
     ]
 
     food_pages = [
-        {'title': 'Pasta Noodles', 'url': 'http://foodURL1', 'price': 3.99, 'seller_id': seller_id},
-        {'title': 'Canned Beets', 'url': 'http://foodURL2', 'price': 2.49, 'seller_id': seller_id}
+        {'title': 'Pasta Noodles', 'url': 'http://foodURL1', 'seller_id': seller_id, 'description': 'Delicious pasta noodles'},
+        {'title': 'Canned Beets', 'url': 'http://foodURL2', 'seller_id': seller_id, 'description': 'Healthy canned beets'}
     ]
     
     cats = {'Stationery': {'pages': stationery_pages},
@@ -51,17 +51,18 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_category(cat)
         for p in cat_data['pages']:
-            add_listing(c, p['title'], p['url'], p['price'], p['seller_id'])
+            add_listing(c, p['title'], p['url'], p['seller_id'], description=p.get('description', ''))
     
     for c in Category.objects.all():
         for p in Listing.objects.filter(category=c):
             print(f'- {c}: {p}')
 
-def add_listing(cat, title, url, price, seller_id, views=0):
-    p = Listing.objects.get_or_create(category=cat, title=title, price=price, seller_id=seller_id)[0]
+def add_listing(cat, title, url, seller_id, views=0, description=""):
+    # retrieve or create the Listing instance.
+    p = Listing.objects.get_or_create(category=cat, title=title, seller_id=seller_id)[0]
     p.url = url
     p.views=views
-    p.price = price
+    p.description = description # new field added
     p.seller = CustomUser.objects.get(pk=seller_id)
     p.save()
     return p
